d411ec7ae5709ad11691fb9a558295faf9e8ebda
Guiu Constantin <Guiu Constantin>
2017-03-21 11:08:30 +0100

    commit_05

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 let name = "Costin";
 
-let numKids = 3;
+let numKids = 1;
+
+let partner = "Ana";
 
 let address = "Knabstrupvej 21";
 
 let hir = "a lot";
 
+let job = "film director";
+
+let age = 20;
+
 numKids = 0;
+
+let info = "You will be a " + job + " in " + address + ", and married to " + partner + " with " + numKids + " kids.";
+console.log(info);
